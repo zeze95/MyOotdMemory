a42ee343fbf9975d57b42b029086d299d4c0c90e
zeze95 <keaje214@gmail.com>
2023-10-17 17:48:41 +0900

    fix err

---
+++ MyOotdMemory/Component/Cell/NodataTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NodataTableViewCell.swift
+//  MyOotdMemory
+//
+//  Created by zeze kim on 10/17/23.
+//
+
+import UIKit
+
+class NodataTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
