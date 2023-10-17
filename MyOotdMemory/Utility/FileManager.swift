bc0df7f8534db4b69ffcfaa33a344bd90df18725
zeze95 <keaje214@gmail.com>
2023-10-17 16:29:47 +0900

    add adviceView items

--- MyOotdMemory/Utility/FileManager.swift
+++ MyOotdMemory/Utility/FileManager.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-extension CreateViewController {
+class ImageDoucment {
 
     func documentDirectoryPath() -> URL? {
         //1. 도큐먼트 경로 찾기
@@ -31,16 +31,16 @@ extension CreateViewController {
     
     func loadImageFromDocument(fileName: String) -> UIImage {
         //1. 도큐먼트 경로 찾기
-        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return UIImage(systemName: "star.fill")! }
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return UIImage(named:"photoimg")!  }
+     
         //2. 저장할 경로 설정(세부 경로, 이미지를 저장되어 있는 위치)
         let fileURL = documentDirectory.appendingPathComponent(fileName)
         
         if FileManager.default.fileExists(atPath: fileURL.path) {
-            return UIImage(contentsOfFile: fileURL.path)! // 시간이 걸리진않음
+            return UIImage(contentsOfFile: fileURL.path)!
         } else {
-            return UIImage(systemName: "star.fill")!
+            return UIImage(named:"photoimg")!
         }
-        
     }
     
     //도큐먼트 폴더에 이미지를 저장하는 메서드
@@ -49,7 +49,7 @@ extension CreateViewController {
         guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return } // 도큐먼트 폴더까지 들어옴
         //2. 저장할 경로 설정(세부 경로, 이미지를 저장할 위치)
         let fileURL = documentDirectory.appendingPathComponent(fileName)
-        guard let data = image.jpegData(compressionQuality: 0.4) else { return }
+        guard let data = image.jpegData(compressionQuality: 0.6) else { return }
         do {
             try data.write(to: fileURL)
         } catch let error {
